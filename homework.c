a92485c0bfc70ac50050922294808c4ce0921f79
liza2281337 <stas1kbro6@gmail.com>
2024-11-17 21:02:56 +0300

    second commit

---
+++ homework.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+unsigned str_len(char* str) {
+    unsigned length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}
+
+int main() {
+    char string[] = "meow";
+    printf("Длина строки \"%s\" = %u\n", string, str_len(string));
+    return 0;
+}
+
